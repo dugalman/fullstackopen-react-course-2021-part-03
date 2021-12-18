9adbedcdcd9e1c52030b82ed9affb346d277dfe0
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2021-12-18 13:17:54 -0300

    uibuild

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
 const dotenv = require('dotenv');
-
 if (process.env && process.env.NODE_ENV) {
   dotenv.config({ path: '.env.' + process.env.NODE_ENV });
 } else {
   dotenv.config();
 }
-
-const express = require('express')
-const cors = require('cors')
 const Note = require('./models/note')
 
-///////////////////////////// EXPRESS ////////////////////////////////////////
-const app = express()
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json()) // parse los request como json
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -20,90 +30,78 @@ const requestLogger = (request, response, next) => {
   console.log('---')
   next()
 }
+
 app.use(requestLogger)
 
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-
-  next(error)
-}
-
-//////////////////////// MIDLEWARE ////////////////////////////////////////////
-
-app.use(express.static('build'))
-app.use(express.json()) // parse los request como json
-
-app.use(cors())
-app.use(errorHandler)
-
-
-///////////////////////////////////////////////////////////////////////////////
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-///////////////////////////////////////////////////////////////////////////////
 app.get('/api/notes', (req, res) => {
-  Note.find({})
-    .then(notes => { res.json(notes) })
-    .catch(error => next(error))
+  Note.find({}).then(notes => { 
+    res.json(notes) 
+  })
 })
 
-///////////////////////////////////////////////////////////////////////////////
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
-  if (!body.content) {
+  if (body.content === undefined) {
     return response.status(400).json({ error: 'content missing' })
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-    date: new Date()
+    date: new Date(),
   })
 
-  note.save()
-    .then(savedNote => { response.json(savedNote) })
+  note.save().then(savedNote => { 
+    response.json(savedNote) 
+  })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note) 
+      } else { 
+        response.status(404).end() 
+      }
+    })
+    .catch(error => {
+      console.log("WAT IS THIS")
+      next(error)
+    })
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
     .catch(error => next(error))
 })
 
-///////////////////////////////////////////////////////////////////////////////
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id)
-    .then(note => {
-      if (note) { response.json(note) }
-      else { response.status(404).end() }
-    })
-    .catch(error => {
-      console.log(error)
-      response.status(400).send({error:'malformatted id'})
-    })
-})
-
-
-
-
-
-///////////////////////////////////////////////////////////////////////////////
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
-})
-
-///////////////////////////////////////////////////////////////////////////////
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
 
+app.use(errorHandler)
 
 ///////////////// MAIN ////////////////////////////////////////////////////////
 const PORT = process.env.PORT || 3001
