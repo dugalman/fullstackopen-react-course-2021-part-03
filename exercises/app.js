0c4623930fe183e1e35be2187aa303e1d2aaf10b
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2021-12-28 18:55:14 -0300

    Se mueve la funcionalidad de manejo de url hacia los controladores

--- exercises/app.js
+++ exercises/app.js
@@ -1,19 +1,14 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
+const blogsRouter = require('./controllers/BlogsController')
+
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
@@ -22,22 +17,6 @@ logger.info(`Connect mongo server ${mongoUrl}`)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.use('/api/blogs',blogsRouter)
 
 module.exports = app
