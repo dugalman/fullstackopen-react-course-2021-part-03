51d53695ea00823e7d1f6979a8e8d713e370c52b
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2021-12-28 18:26:11 -0300

    ident with 4 space

--- exercises/index.js
+++ exercises/index.js
@@ -6,5 +6,5 @@ const logger = require('./utils/logger')
 const PORT = config.PORT || 3003
 
 app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+    logger.info(`Server running on port ${PORT}`)
 }) 
