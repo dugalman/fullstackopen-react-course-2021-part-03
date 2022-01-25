06f529a4c8023e966152e647f4a16ea6e3857e0b
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-01-25 17:16:35 -0300

    end part 4-4

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,9 +1,9 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', async (request, response) => {
+notesRouter.get('/', async (request, response) => { 
   const notes = await Note.find({})
-  response.json(notes)
+  response.json(notes.map(note => note.toJSON()))
 })
 
 notesRouter.post('/', async (request, response, next) => {
@@ -11,37 +11,41 @@ notesRouter.post('/', async (request, response, next) => {
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
-    date: new Date()
+    important: body.important === undefined ? false : body.important,
+    date: new Date(),
   })
 
-  try {
+  try { 
     const savedNote = await note.save()
     response.json(savedNote.toJSON())
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+notesRouter.get('/:id', async (request, response, next) => {
+  try{
+    const note = await Note.findById(request.params.id)
+    if (note) {
+      response.json(note.toJSON())
+    } else {
+      response.status(404).end()
+    }
+  } catch(exception) {
+    next(exception)
+  }
+})
+
+notesRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
   } catch (exception) {
     next(exception)
   }
-
-
 })
 
-notesRouter.get('/:id', async (request, response) => {
-
-  const note = await Note.findById(request.params.id)
-  if (note) {
-    response.json(note.toJSON())
-  } else {
-    response.status(404).end()
-  }
-
-})
-
-notesRouter.delete('/:id', async (request, response) => {
-  await Note.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-})
-
-notesRouter.put('/:id', async (request, response) => {
+notesRouter.put('/:id', (request, response, next) => {
   const body = request.body
 
   const note = {
@@ -49,8 +53,11 @@ notesRouter.put('/:id', async (request, response) => {
     important: body.important,
   }
 
-  const updatedNote = Note.findByIdAndUpdate(request.params.id, note, { new: true })
-  response.json(updatedNote.toJSON())
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote.toJSON())
+    })
+    .catch(error => next(error))
 })
 
 module.exports = notesRouter
