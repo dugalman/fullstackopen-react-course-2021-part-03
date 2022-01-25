f8b4de150e39c829709955839af8469b5000d117
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-01-25 16:59:11 -0300

    fin 4-3

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,10 +1,10 @@
-const supertest = require('supertest')
 const mongoose = require('mongoose')
-const helper = require('./test_helper')
+const supertest = require('supertest')
 
 const app = require('../app')
 const api = supertest(app)
 
+const helper = require('./test_helper')
 const Note = require('../models/note')
 
 
