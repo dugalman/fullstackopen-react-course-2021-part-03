a67854098325fabd5a87cde876a89b8e4639d94f
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-02-05 18:45:45 -0300

    end exercise 4.14

--- exercises/models/Blog.js
+++ exercises/models/Blog.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
 
 const blogSchema = new mongoose.Schema({
-    title: { type: String, minlength: 3, required: true, unique: true },
-    author: { type: String, required: true },
-    url: { type: String, minlength: 3, required: true },
-    likes: { type: Number }
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
 })
+
 
 blogSchema.set('toJSON', {
     transform: (document, returnedObject) => {
