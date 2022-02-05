cd5221fa9e202c975d8631c612ca931abd33b745
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-02-05 15:46:33 -0300

    refactor de las pruebas de note

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
-
+const helper = require('./test_helper')
 const app = require('../app')
 const api = supertest(app)
 
-const helper = require('./test_helper')
 const Note = require('../models/note')
 
 
@@ -33,43 +32,75 @@ beforeEach(async () => {
   await Note.deleteMany({})
   const noteObjects = helper.initialNotes.map(note => new Note(note))
   const promiseArray = noteObjects.map(note => note.save())
-  
+
   /**
    * Aún se puede acceder a los valores devueltos de cada promesa en la matriz cuando se usa el método Promise.all. Si esperamos a que se resuelvan las promesas con la sintaxis "await const results = await Promise.all (promiseArray)"", la operación devolverá una matriz que contiene los valores resueltos para cada promesa en promiseArray, y aparecen en el mismo orden que las promesas en la matr
    */
   await Promise.all(promiseArray)
 })
 
-
-describe('PART 04 :api ', () => {
-
+describe('when there is initially some notes saved', () => {
   test('notes are returned as json', async () => {
     await api
       .get('/api/notes')
       .expect(200)
-      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect('Content-Type', /application\/json/)
   })
 
   test('all notes are returned', async () => {
     const response = await api.get('/api/notes')
+
     expect(response.body).toHaveLength(helper.initialNotes.length)
-  })
-
-
-  test('the first note is about HTTP methods', async () => {
-    const response = await api.get('/api/notes')
-    expect(response.body[0].content).toBe('HTML is easy')
   })
 
   test('a specific note is within the returned notes', async () => {
     const response = await api.get('/api/notes')
 
     const contents = response.body.map(r => r.content)
-    expect(contents).toContain('Browser can execute only Javascript')
+
+    expect(contents).toContain(
+      'Browser can execute only Javascript'
+    )
+  })
+})
+
+describe('viewing a specific note', () => {
+  test('succeeds with a valid id', async () => {
+    const notesAtStart = await helper.notesInDb()
+
+    const noteToView = notesAtStart[0]
+
+    const resultNote = await api
+      .get(`/api/notes/${noteToView.id}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      
+    const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+
+    expect(resultNote.body).toEqual(processedNoteToView)
   })
 
-  // Escribamos una prueba que agregue una nueva nota y verifique que la cantidad de notas devueltas por la API aumente y que la nota recién agregada esté en la lista.
-  test('a valid note can be added', async () => {
+  test('fails with statuscode 404 if note does not exist', async () => {
+    const validNonexistingId = await helper.nonExistingId()
+
+    console.log(validNonexistingId)
+
+    await api
+      .get(`/api/notes/${validNonexistingId}`)
+      .expect(404)
+  })
+
+  test('fails with statuscode 400 id is invalid', async () => {
+    const invalidId = '5a3d5da59070081a82a3445'
+
+    await api
+      .get(`/api/notes/${invalidId}`)
+      .expect(400)
+  })
+})
+
+describe('addition of a new note', () => {
+  test('succeeds with valid data', async () => {
     const newNote = {
       content: 'async/await simplifies making async calls',
       important: true,
@@ -81,15 +112,17 @@ describe('PART 04 :api ', () => {
       .expect(200)
       .expect('Content-Type', /application\/json/)
 
+
     const notesAtEnd = await helper.notesInDb()
     expect(notesAtEnd).toHaveLength(helper.initialNotes.length + 1)
 
     const contents = notesAtEnd.map(n => n.content)
-    expect(contents).toContain('async/await simplifies making async calls')
+    expect(contents).toContain(
+      'async/await simplifies making async calls'
+    )
   })
 
-  // Escribamos también una prueba que verifique que una nota sin contenido no se guardará en la base de datos.
-  test('note without content is not added', async () => {
+  test('fails with status code 400 if data invaild', async () => {
     const newNote = {
       important: true
     }
@@ -100,40 +133,30 @@ describe('PART 04 :api ', () => {
       .expect(400)
 
     const notesAtEnd = await helper.notesInDb()
+
     expect(notesAtEnd).toHaveLength(helper.initialNotes.length)
   })
 })
 
-// A continuación, escribamos pruebas para obtener una nota individual:
-test('a specific note can be viewed', async () => {
-  const notesAtStart = await helper.notesInDb()
+describe('deletion of a note', () => {
+  test('succeeds with status code 204 if id is valid', async () => {
+    const notesAtStart = await helper.notesInDb()
+    const noteToDelete = notesAtStart[0]
 
-  const noteToView = notesAtStart[0]
+    await api
+      .delete(`/api/notes/${noteToDelete.id}`)
+      .expect(204)
 
-  const resultNote = await api
-    .get(`/api/notes/${noteToView.id}`)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+    const notesAtEnd = await helper.notesInDb()
 
-  const processedNoteToView = JSON.parse(JSON.stringify(noteToView))
+    expect(notesAtEnd).toHaveLength(
+      helper.initialNotes.length - 1
+    )
 
-  expect(resultNote.body).toEqual(processedNoteToView)
-})
+    const contents = notesAtEnd.map(r => r.content)
 
-// A continuación, escribamos pruebas para eliminar una nota individual:
-test('a note can be deleted', async () => {
-  const notesAtStart = await helper.notesInDb()
-  const noteToDelete = notesAtStart[0]
-
-  await api
-    .delete(`/api/notes/${noteToDelete.id}`)
-    .expect(204)
-
-  const notesAtEnd = await helper.notesInDb()
-  expect(notesAtEnd).toHaveLength(helper.initialNotes.length - 1)
-
-  const contents = notesAtEnd.map(r => r.content)
-  expect(contents).not.toContain(noteToDelete.content)
+    expect(contents).not.toContain(noteToDelete.content)
+  })
 })
 
 ////////////////////////////////////////////////
