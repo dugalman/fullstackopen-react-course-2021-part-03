486161952a7f24b030153c594f1a2f4fee6b8988
Damián Mac Dougall <dmacdougall@teceng-gaming.com.ar>
2022-02-09 09:26:19 -0300

    4.c # comienza en uso de usuarios

--- models/note.js
+++ models/note.js
@@ -2,8 +2,12 @@ const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
   content: { type: String, minlength: 5, required: true },
-  date: { type: Date, required: true },
+  date: { type: Date},
   important: { type: Boolean },
+  user:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  }
 })
 
 noteSchema.set('toJSON', {
